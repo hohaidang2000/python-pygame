9108b451491554c910e2045fea87770f81bd55a6
hohaidangpro <72792170+hohaidangpro@users.noreply.github.com>
2021-01-12 21:57:43 +0700

    fix level not loaded

---
+++ img/Kenny Topdown Pack.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tilesheet_complete" tilewidth="64" tileheight="64" tilecount="540" columns="27">
+ <image source="../PNG/Tilesheet/tilesheet_complete.png" width="1728" height="1280"/>
+</tileset>
---
+++ img/tilesheet_complete_2X.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tilesheet_complete_2X" tilewidth="64" tileheight="64" spacing="10" tilecount="1564" columns="46">
+ <image source="../PNG/Tilesheet/tilesheet_complete_2X.png" width="3456" height="2560"/>
+</tileset>
